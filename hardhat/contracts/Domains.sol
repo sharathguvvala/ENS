25d3cd3572165e60e1345a4d41f1959a984f1d59
sharathguvvala <guvvalasharath@gmail.com>
2022-06-27 21:45:09 +0530

    initial

---
+++ hardhat/contracts/Domains.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Domains {
+
+    constructor() {
+        console.log("Deploying the ENS Domains Smart Contract!");
+    }
+}
---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,13 @@
+async function main() {
+  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const domainContract = await domainContractFactory.deploy();
+  await domainContract.deployed();
+  console.log("Contract deployed to:", domainContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
