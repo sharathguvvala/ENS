a6c1f551288bd8aef069f8c4d27882440ee4f3cb
sharathguvvala <guvvalasharath@gmail.com>
2022-06-30 23:09:34 +0530

    smart contract

--- hardhat/contracts/Domains.sol
+++ hardhat/contracts/Domains.sol
@@ -5,7 +5,15 @@ import "hardhat/console.sol";
 
 contract Domains {
 
+    mapping(string=>address) public domains;
+
     constructor() {
         console.log("Deploying the ENS Domains Smart Contract!");
+    }
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+    }
+    function getAddress(string calldata name) public view returns(address) {
+        return domains[name];
     }
 }
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,8 +1,14 @@
 async function main() {
-  const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+  const [owner, randomUser] = await hre.ethers.getSigners();
+  const domainContractFactory = await hre.ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
-  console.log("Contract deployed to:", domainContract.address);
+  console.log("contract deployed to:", domainContract.address);
+  console.log("contract deployed by:", owner.address);
+  const txn = await domainContract.register("hello");
+  await txn.wait();
+  const domainOwner = await domainContract.getAddress("hello");
+  console.log("owner of domain:",domainOwner);
 }
 
 main()
