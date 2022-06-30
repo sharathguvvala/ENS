a63595115472111b10cf8cccc346bd89a950b487
sharathguvvala <guvvalasharath@gmail.com>
2022-07-01 01:08:33 +0530

    deployed

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,20 +1,12 @@
 async function main() {
-    //const [owner, randomUser] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory("Domains");
     const domainContract = await domainContractFactory.deploy("stacex");
     await domainContract.deployed();
     console.log("contract deployed to:", domainContract.address);
-    // console.log("contract deployed by:", owner.address);
-    // let txn = await domainContract.register("hello");
-    // await txn.wait();
-    // const domainOwner = await domainContract.getAddress("hello");
-    // console.log("owner of domain:",domainOwner);
-    // txn = await domainContract.connect(randomUser).setRecord("hello","contract address");
-    // await txn.wait();
-    let txn = await domainContract.register("immortal", {value: hre.ethers.utils.parseEther("0.1")});
+    let txn = await domainContract.register("sharath", {value: hre.ethers.utils.parseEther("0.1")});
     await txn.wait();
-    const address = await domainContract.getAddress("immortal");
-    console.log("owner of domain immortal:", address);
+    const address = await domainContract.getAddress("sharath");
+    console.log("owner of domain sharath:", address);
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("contract balance:", hre.ethers.utils.formatEther(balance));
 }
