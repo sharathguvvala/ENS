7554660dd7ad4b4f8ac39d9dd077db6baac5d9f2
sharathguvvala <guvvalasharath@gmail.com>
2022-07-01 23:26:25 +0530

    wallect connection

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,8 +1,43 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {ethers} from 'ethers'
+import Web3Modal from 'web3modal'
+import { useState } from 'react'
+
+let web3Modal
+if(typeof window !== 'undefined'){
+  web3Modal = new Web3Modal({
+    network: 'mumbai',
+    providerOptions: {},
+    disableInjectedProvider: false
+  })
+}
 
 export default function Home() {
+
+  const [walletConnected,setWalletConnected] = useState(false)
+
+  const connectWallet = async(needSigner=false) => {
+    try {
+      const instance = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(instance)
+      const {chainId} = await provider.getNetwork()
+      if(chainId!==80001){
+        window.alert("connect with polygon mumbai testnet")
+      }
+      if(needSigner){
+        const signer = await provider.getSigner()
+        setWalletConnected(true)
+        return signer
+      }
+      setWalletConnected(true)
+        return provider
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +47,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <button>Connect Wallet</button>
+        <button onClick={connectWallet} >Connect Wallet</button>
       </main>
 
       <footer className={styles.footer}>
